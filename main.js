11e2a9c9a4f846814cf1ee3c048b5400d9b0a8e7
Ivy <yunjilim@gmail.com>
2019-08-20 13:12:34 -0700

    semantic html setup

---
+++ main.js
@@ -0,0 +1,12 @@
+/*----- constants -----*/
+
+
+/*----- cached element references -----*/
+const playButton = document.querySelector('.button');
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
